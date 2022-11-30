fd84ad9728df75be760bee10900bd006bc7480dc
rubenconde1 <rubencnd96@gmail.com>
2022-11-30 09:17:12 +0100

    Primeros pasos pdf terminado

---
+++ symfony-tienda/src/Controller/PageController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    #[Route('/price', name: 'price')]
+    public function price(): Response
+    {
+        return $this->render('page/price.html.twig', []);
+    }
+
+    #[Route('/service', name: 'service')]
+    public function service(): Response
+    {
+        return $this->render('page/service.html.twig', []);
+    }
+
+    #[Route('/team', name: 'team')]
+    public function team(): Response
+    {
+        return $this->render('page/team.html.twig', []);
+    }
+
+    #[Route('/testimonial', name: 'testimonial')]
+    public function testimonial(): Response
+    {
+        return $this->render('page/testimonial.html.twig', []);
+    }
+}
